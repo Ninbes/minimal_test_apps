d7d13cd38c0f418aee72cad8ae9797f8f2e2b87e
Bence <pbence.mate@gmail.com>
2024-08-06 16:41:47 +0200

    .

--- pong/app.py
+++ pong/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import socket
 import redis
 
 app = Flask(__name__)
+CORS(app)
 
 # Connect to Redis
 redis_host = os.getenv('REDIS_HOST', 'localhost')
